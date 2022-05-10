3cd1d8bc5b32b5d7589798e7eccb7a8b26fb02bf
Amir Ali <amirliaqat2020@gmail.com>
2022-05-10 13:18:05 +0500

    Adding standard structure files.

---
+++ bb-live-chat.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name:       ByteBunch Live Chat
+Plugin URI:          https://bytebunch.com/plugin
+Description:        This is my first testing plugin.
+Version:               1.0.0
+Requires at least: 5.2
+Requires PHP:      8.1.2
+Author:               ByteBunch
+Author URI:        https://bytebunch.com
+License:               GPL v2 or later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:       bb_live_chat
+Domain Path:       /languages
+*/

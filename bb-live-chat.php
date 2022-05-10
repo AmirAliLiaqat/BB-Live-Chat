76e753a0255eca509c408f316e1d31e56edd4048
Amir Ali <amirliaqat2020@gmail.com>
2022-05-10 14:28:12 +0500

    Creating constants for plugin path, url & directory.

--- bb-live-chat.php
+++ bb-live-chat.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:       ByteBunch Live Chat
 Plugin URI:          https://bytebunch.com/plugin
-Description:        This is my first testing plugin.
+Description:        This is wordpress plugin which is created for the wordpress site to live chat with admin and other staff members.
 Version:               1.0.0
 Requires at least: 5.2
 Requires PHP:      8.1.2
@@ -14,3 +14,23 @@ License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:       bb_live_chat
 Domain Path:       /languages
 */
+
+// If anyone can access this file directory show error.
+if ( ! defined( 'ABSPATH' ) ) {
+    die;
+}
+
+// Define CONSTANT for plugin directory path.
+if ( ! defined( 'PLUGIN_DIR_PATH' ) ) {
+    define( 'PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+// Define CONSTANT for plugin directory url.
+if ( ! defined( 'PLUGIN_DIR_URL' ) ) {
+    define( 'PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
+}
+
+// Define CONSTANT for plugin directory.
+if ( ! defined( 'PLUGIN' ) ) {
+    define( 'PLUGIN', plugin_basename( __FILE__ ) );
+}
